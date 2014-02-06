11ac7e70b7e2273e1fc5c639f5e281da2609c571
Luis A. Echeverria <pixelsnob@gmail.com>
2014-02-05 23:14:43 -0800

    Added less files

--- app.js
+++ app.js
@@ -6,18 +6,7 @@ var
   app             = express(),
   mongoose        = require('mongoose'),
   RedisStore      = require('connect-redis')(express),
-  _               = require('underscore'),
-  DB_OPTS = {
-    server: {
-      auto_reconnect: true,
-      socketOptions: {
-        keepAlive: 1,
-        connectTimeoutMS: 10000
-      }
-    }
-  },
-  DB_URI          = 'mongodb://localhost/lapr',
-  db              = mongoose.connect(DB_URI, DB_OPTS);
+  _               = require('underscore');
 
 app.configure('development', function() {
   app.use(express.static(__dirname + '/public'));
@@ -33,19 +22,6 @@ app.configure(function() {
   app.use(express.json());
   //app.use(express.cookieParser());
   //app.use(express.session({ store: new RedisStore, secret: 'hotdog' }));
-});
-
-db.connection.once('connected', function() {
-  console.log('mongo connected');
-});
-db.connection.on('error', function(err) {
-  console.error('mongo error: ' + err);
-});
-db.connection.on('disconnected', function(err) {
-  console.log('mongo disconnected');
-});
-db.connection.on('reconnected', function() {
-  console.log('mongo reconnected');
 });
 
 // Routes
