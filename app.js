5bcf8cc71dd84a115aa731a4a38a186f16a46f67
Luis A. Echeverria <pixelsnob@gmail.com>
2014-02-17 16:06:12 -0800

    More markdown changes

--- app.js
+++ app.js
@@ -7,28 +7,46 @@ var
   fs              = require('fs'),
   express         = require('express'),
   app             = express(),
-  markdown        = require('marked'),
+  marked          = require('marked'),
+  markdown        = require('markdown').markdown,
+  jade            = require('jade'),
   mongoose        = require('mongoose'),
   RedisStore      = require('connect-redis')(express),
   _               = require('underscore'),
-  exec            = require('child_process').exec;
+  child_process   = require('child_process'),
+  exec_sync       = require('execSync'),
+  util            = require('util');
 
-markdown.setOptions({
+/*marked.setOptions({
   gfm: true,
+  breaks: true,
   tables: true,
   sanitize: false,
   smartypants: false
-});
+});*/
+
+// Intercept markdown filter
+jade.filters.md = function(text, opts) {
+  // return markdown.toHTML(text, 'Gruber');
+  //return marked(text);
+  var ret = exec_sync.exec('pandoc ' + opts.filename)
+  if (ret.code !== 0) {
+    throw new Error(ret.stdout);
+    return;
+  }
+  return ret.stdout;
+};
 
 app.configure(function() {
   app.set('view engine', 'jade');
   app.set('views', views_dir);
-  app.set('view cache', false);
+  app.set('view cache', true);
   app.locals.pretty = true;
   app.locals._ = _;
   app.use(express.urlencoded()); 
   app.use(express.json());
   app.use(express.static('public'));
+  app.use(express.json());
 });
 
 app.get('/', function(req, res, next) {
@@ -37,7 +55,7 @@ app.get('/', function(req, res, next) {
 
 // Keeps files in sync with master
 app.post('/sync', function(req, res, next) {
-  exec('bin/sync', function(err, stdout, stderr) {
+  child_process.exec('bin/sync', function(err, stdout, stderr) {
     if (err) {
       console.log(err);
       return res.send(500);
--- views/content/sounds/trios-or-larger.md
+++ views/content/sounds/trios-or-larger.md
@@ -1,6 +1,3 @@
 
 # Trios or Larger
 
-<object width="445" height="364"><param name="movie" value="http://www.youtube-nocookie.com/v/Y2uwa6ahyks&hl=en&fs=1&color1=0x3a3a3a&color2=0x999999&border=1" /><param name="allowFullScreen" value="true" /><param name="allowscriptaccess" value="always" />
-   <embed src="http://www.youtube-nocookie.com/v/Y2uwa6ahyks&hl=en&fs=1&color1=0x3a3a3a&color2=0x999999&border=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="445" height="364"></embed>
-</object>
