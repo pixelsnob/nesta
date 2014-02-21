df8970a62008f19ae42c330f5f4a8bee28a6f10a
Luis A. Echeverria <pixelsnob@gmail.com>
2014-02-21 13:29:50 -0800

    Added cms files

--- app.js
+++ app.js
@@ -7,13 +7,18 @@ var
   fs              = require('fs'),
   express         = require('express'),
   app             = express(),
+  routes          = require('./routes')(app),
   marked          = require('marked'),
   jade            = require('jade'),
-  mongoose        = require('mongoose'),
-  RedisStore      = require('connect-redis')(express),
+  redis_store     = require('connect-redis')(express),
+  passport        = require('passport'),
   _               = require('underscore'),
   child_process   = require('child_process'),
-  util            = require('util');
+  util            = require('util'),
+  //mongoose        = require('mongoose'),
+  db              = require('./db');
+
+require('./auth');
 
 marked.setOptions({
   gfm: true,
@@ -23,33 +28,55 @@ marked.setOptions({
   smartypants: true
 });
 
-/*
-// Intercept markdown filter
-jade.filters.md = function(text, opts) {
-  return marked(text);
-};
-*/
-
 app.configure(function() {
   app.set('view engine', 'jade');
   app.set('views', views_dir);
-  app.set('view cache', false);
-  app.locals.pretty = true;
-  app.locals._ = _;
+  app.set('view cache', true);
   app.use(express.urlencoded()); 
   app.use(express.json());
+  app.use(express.cookieParser());
+  app.use(express.session({ store: new redis_store, secret: 'hot~dog' }));
   app.use(express.static('public'));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.locals.pretty = true;
+  app.locals._ = _;
+  app.locals.markdown = marked;
+  app.locals.renderContentBlock = function(name, page) {
+    var content_block = _.findWhere(page.content_blocks, { name: name });
+    if (content_block && content_block.content) {
+      return marked(content_block.content);
+    }
+  };
+});
+
+/*app.get('/', function(req, res, next) {
+  res.render('index');
 });
 
 app.get('/', function(req, res, next) {
-  res.render('index');
-});
+  res.send(JSON.stringify(req.user));
+});*/
+
+app.get('/login', routes.loginForm);
+app.post('/login', routes.login);
+app.get('/logout', routes.logout);
+
+// CMS dynamic routes
+app.get('*', routes.renderCmsPage);
+app.put(
+  '*',
+  routes.auth,
+  routes.saveCmsPage,
+  routes.saveCmsContentBlocks,
+  routes.renderCmsPage
+);
 
 // Keeps files in sync with master
 app.post('/sync', function(req, res, next) {
   child_process.exec('bin/sync', function(err, stdout, stderr) {
     if (err) {
-      console.log(err);
+      console.error(err);
       return res.send(500);
     }
     console.log(stdout);
@@ -69,7 +96,7 @@ app.get('*', function(req, res, next) {
 });
 
 // Error page
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
   console.log(err.stack);
   res.format({
     html: function() {
