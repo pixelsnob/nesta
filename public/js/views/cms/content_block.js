65ef04f08b5acbb2105f7864e4b5a825f4318176
Luis A. Echeverria <pixelsnob@gmail.com>
2014-02-26 17:29:10 -0800

    Tweak to "cancel" functionality

--- public/js/views/cms/content_block.js
+++ public/js/views/cms/content_block.js
@@ -23,12 +23,14 @@ define([
       'click .save a':             'save',
       'click .cancel a':           'cancel'
     },
+    
     initialize: function(opts) {
       this.editor_tpl = $(jade.render('cms_content_block_editor'));
       this.listenTo(this.model, 'change', function(model) {
         this.render();
       });
     },
+    
     edit: function(ev) {
       if (this.$el.hasClass('editing')) {
         return false;
@@ -43,6 +45,7 @@ define([
       textarea.get(0).focus();
       return false;
     },
+    
     save: function(ev) {
       // Must clone so that change events will fire correctly
       var content_block = _.clone(this.model.get('content_block'));
@@ -52,22 +55,27 @@ define([
         this.render();
       }
     },
+    
     cancel: function(ev) {
-      ///if (!this.model.hasChanged()) {
-      //  return this.render();
-     // }
+      var has_not_changed = (this.model.get('content_block').content ==
+                            this.$el.find('textarea').val());
+      if (has_not_changed) {
+        return this.render();
+      }
       if (window.confirm('Unsaved changes will be lost! Continue?')) {
         this.render();
       } else {
         this.$el.find('textarea').get(0).focus();
       }
     },
+
     keyup: function(ev) {
       // Cancel button
       if (ev.keyCode == 27) {
         this.cancel(ev);
       }
     },
+    
     render: function() {
       var content = this.model.get('content_block').content;
       if (this.model.get('content_block').type == 'markdown') {
