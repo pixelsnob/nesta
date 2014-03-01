b89d0a41324d5bf672700ca7adc80cc841781f24
Luis A. Echeverria <pixelsnob@gmail.com>
2014-02-28 23:42:32 -0800

    Markdown image stuff

--- public/js/views/cms/content_block_editor.js
+++ public/js/views/cms/content_block_editor.js
@@ -14,7 +14,8 @@ define([
   return Backbone.View.extend({
     model: new ContentBlockModel,
     events: {
-      'blur textarea':             'save',
+      'click .editor_close':       'save',
+      'click textarea':            'selectImage',
       'keyup':                     'keyup'
     },
     
@@ -28,10 +29,10 @@ define([
         .width(this.$el.width())
         .height(window.document.documentElement.clientHeight - 200);
       var textarea = this.template.find('textarea')
-        .val(this.model.get('content_block').content);
+        .val(this.model.get('content_block').content).get(0);
       this.$el.empty();
       this.$el.append(this.template);
-      textarea.get(0).focus();
+      textarea.focus();
       return this.$el;
     },
     
@@ -44,12 +45,41 @@ define([
         this.trigger('saved');
       }
     },
+    
+    selectImage: function() {
+      var textarea = this.template.find('textarea'),
+        text       = textarea.val(),
+        // Get markdown images, format ![Name](path/to/image)
+        images     = text.match(/!\[[^\]]*\]\([^\)]*\)/g),
+        sel_start  = textarea.prop('selectionStart'),
+        sel_end    = textarea.prop('selectionEnd'),
+        image      = '';
+
+      for (var i in images) {
+        // Get start and end position
+        var img_start = text.indexOf(images[i]),
+            img_end   = img_start + images[i].length;
+        // See if the cursor is between (or next to one of) the two positions
+        // and select
+        if (sel_start >= img_start && sel_end <= img_end) {
+          //textarea.get(0).setSelectionRange(img_start, img_end);
+          image = images[i];
+        }
+      }
+      // Add check to see if this image is already showing
+      this.$el.find('.image').empty();
+      if (image) {
+        var path = image.match(/\(([^\)]*)\)/);
+        this.$el.find('.image').append($('<img>').attr('src', path[1]));
+      }
+    },
 
     keyup: function(ev) {
       // Cancel button
       if (ev.keyCode == 27) {
-        this.save();
+        return this.save();
       }
+      this.selectImage();
     }
   });
 });
