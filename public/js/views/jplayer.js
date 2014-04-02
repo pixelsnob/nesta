e77c9d902a773109c8040b0a261e17d5cb881783
Luis A. Echeverria <pixelsnob@gmail.com>
2014-04-01 20:14:49 -0700

    More jplayer tweaks

--- public/js/views/jplayer.js
+++ public/js/views/jplayer.js
@@ -35,8 +35,10 @@ define([
     play: function(model) {
       if (model.get('file_type') == 'video') {
         this.$player_container.height(300);
+        this.$player.height(270);
       } else {
         this.$player_container.height(0);
+        this.$player.height(0);
       }
       var opts = {};
       opts[model.get('jplayer_type')] = model.get('src');
@@ -45,13 +47,18 @@ define([
     },
     
     stop: function() {
-      this.$player.jPlayer('stop');
       this.$player_container.height(0);
+      this.$player.height(0);
+      this.$player.jPlayer('stop');
+      this.$player.jPlayer('clearMedia');
       this.trigger('stop');
     },
     
     ended: function() {
       this.$player_container.height(0);
+      this.$player.height(0);
+      this.$player.jPlayer('clearMedia');
+      console.log('ended');
       this.trigger('ended');
     }
   });
