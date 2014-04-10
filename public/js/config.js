937a768dd14174f01d167bcab81031112e7ff734
Luis A. Echeverria <pixelsnob@gmail.com>
2014-04-09 22:50:24 -0700

    Gruntfile change

--- public/js/config.js
+++ public/js/config.js
@@ -12,13 +12,11 @@ require.config({
     bootstrap:        '../bower_components/bootstrap/dist/js/bootstrap',
     jplayer:          '../bower_components/jplayer/jquery.jplayer/jquery.jplayer',
     youtube:          'https://www.youtube.com/iframe_api?noext'
-    //html5media:       '../bower_components/html5media/cdn/www/1.1.7/html5media.min'
   },
   shim: {
     jade:              { exports: 'jade' },
     'backbone-forms':  { deps: [ 'backbone' ] },
     bootstrap:         { deps: [ 'jquery' ], exports: '$' },
-    //html5media:        { exports: 'html5media' },
     jplayer:           { deps: [ 'jquery' ] },
     youtube:           { exports: 'YT' }
   }
