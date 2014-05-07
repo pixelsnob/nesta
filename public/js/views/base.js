91c7323b6b094f51767919462d37fe230d7a5430
Luis A. Echeverria <pixelsnob@gmail.com>
2014-05-07 10:36:53 -0700

    Cleanup, frontend bug fix

--- public/js/views/base.js
+++ public/js/views/base.js
@@ -6,7 +6,6 @@ define([ 'backbone' ], function(Backbone) {
   return Backbone.View.extend({
 
     showServerError: function(model, xhr) {
-      console.log(this);
       if (xhr.status == 403) {
         alert('You must be logged in to do that...');
       } else {
