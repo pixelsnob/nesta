1183bb79b81da1fb7bdd5e7f4c570ce344d51537
Luis A. Echeverria <pixelsnob@gmail.com>
2014-06-08 18:20:33 -0700

    More tests

--- public/js/test/spec/cms/views/page_options.js
+++ public/js/test/spec/cms/views/page_options.js
@@ -14,26 +14,24 @@ define([
     beforeEach(function() {
       model = new PageModel(fixtures.page);
       view = new PageOptionsView({ model: model });
+      view.render();
     });
     describe('When render() is called', function() {
       it('should render a form with the correct form elements', function() {
-        var $el = view.render().$el;
-        var $title = $el.find('textarea[name=title]')
+        var $title = view.form.$el.find('textarea[name=title]')
         expect($title.length).toBe(1);
         expect($title.val()).toBe(fixtures.page.title);
       });
     });
     describe('When form is committed with correct values', function() {
       it('should not error', function() {
-        view.render();
         var errors = view.form.commit();
         expect(errors).toBeUndefined();
       });
     });
     describe('When form with a missing value is committed', function() {
       it('should give a "required" error', function() {
-        var $el = view.render().$el;
-        var $title = $el.find('textarea[name=title]');
+        var $title = view.form.$el.find('textarea[name=title]');
         $title.val('');
         var errors = view.form.commit();
         expect(errors.title.message).not.toBeUndefined();
@@ -41,8 +39,7 @@ define([
     });
     describe('When form is committed with a newline ', function() {
       it('should give a "no newlines" error', function() {
-        var $el = view.render().$el;
-        var $description = $el.find('textarea[name=description]');
+        var $description = view.form.$el.find('textarea[name=description]');
         $description.val("\n");
         var errors = view.form.commit();
         expect(errors.description.message).not.toBeUndefined();
