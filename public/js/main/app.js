d7372ef521395e0ce8fde0bfc4aff88d81a539eb
Luis A. Echeverria <pixelsnob@gmail.com>
2014-07-14 18:47:25 -0700

    Removed reference to NavView in app.js

--- public/js/main/app.js
+++ public/js/main/app.js
@@ -8,7 +8,6 @@ define([
 ], function(Backbone, csrf, AppView, NavView) {
   $(function() {
     new AppView;
-    new NavView;
     Backbone.history.start({
       pushState: true,
       hashChange: false,
