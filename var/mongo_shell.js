718c7f509eb2688b091cc3421106017b872ebe39
Luis A. Echeverria <pixelsnob@gmail.com>
2014-08-08 00:42:41 -0700

    Content themes css refactoring

--- var/mongo_shell.js
+++ var/mongo_shell.js
@@ -4,3 +4,7 @@ db.pages.update({ path: 'about' }, { $pop: { content_blocks: 1 }});
 db.pages.insert({ path: 'sounds/what-size-band-do-i-choose', title: 'What Size Band Do I Choose?', description: '', keywords: '', view: 'cms/pages/default', content_blocks: [{ _id: (new ObjectId), name: 'main', content: '# What Size Band Do I Choose?', type: 'markdown' }], body_class: 'band-size' });
 
 db.pages.update({ path: /sounds\/*/, 'content_blocks.name': 'main' }, { $set: { 'content_blocks.$.class_names': 'photos' }}, { multi: true });
+
+
+db.pages.update({ path: /sounds\/trios-and-larger/, 'content_blocks.name': 'main' }, { $set: { 'content_blocks.$.class_names': 'photos' }}, { multi: false });
+db.pages.update({ path: /testimonials|faq/, 'content_blocks.name': 'main' }, { $set: { 'content_blocks.$.class_names': 'list-boxes' }}, { multi: true });
