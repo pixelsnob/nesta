4964ec02bb232a0161c564a37257f9cf06ee7453
Luis A. Echeverria <pixelsnob@gmail.com>
2014-08-24 17:47:29 -0700

    Debugging mongo connection errors, backup script change

--- config/db.js
+++ config/db.js
@@ -5,11 +5,11 @@ var mongoose = require('mongoose');
 
 var db_opts = {
   server: {
-    auto_reconnect: true,
+    /*auto_reconnect: true,
     socketOptions: {
       keepAlive: 1,
       connectTimeoutMS: 10000
-    }
+    }*/
   }
 };
 
@@ -20,7 +20,7 @@ db.connection.once('connected', function() {
   console.log('mongo connected');
 });
 db.connection.on('error', function(err) {
-  console.error('mongo error: ' + err);
+  console.error('mongo error', err);
 });
 db.connection.on('disconnected', function(err) {
   console.log('mongo disconnected');
