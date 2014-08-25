676cea235de06e26f3a838c5ea5d1368e4e6935e
Luis A. Echeverria <pixelsnob@gmail.com>
2014-08-24 19:55:22 -0700

    Moved public/js/main/lib to public/js/lib

--- public/js/config.js
+++ public/js/config.js
@@ -13,10 +13,10 @@ define([], function() {
       bootstrap:        '../bower_components/bootstrap/dist/js/bootstrap',
       jplayer:          '../bower_components/jplayer/jquery.jplayer/jquery.jplayer',
       youtube:          'https://www.youtube.com/iframe_api?noext',
-      lib:              'main/lib',
+      //lib:              'main/lib',
       'jquery-ui':      '../bower_components/jquery-ui',
       slider:           '../bower_components/jquery-ui/ui/slider',
-      template:         'main/lib/template'
+      template:         'lib/template'
     },
     shim: {
       jade:              { exports: 'jade' },
