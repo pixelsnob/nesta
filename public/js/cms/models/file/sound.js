2f058dffe351da3ebaf8afa7279d7243c76d99c6
Luis A. Echeverria <pixelsnob@gmail.com>
2014-09-02 14:37:03 -0700

    Added illegal char validator to files

--- public/js/cms/models/file/sound.js
+++ public/js/cms/models/file/sound.js
@@ -19,6 +19,9 @@ define([
     },
     
     validate: function(attrs, opts) {
+      if (!attrs.file.name.match(/^[a-z0-9\-\._]+$/i)) {
+        return 'Illegal characters';
+      }
       if (_.indexOf(this.types, attrs.file.type) === -1) {
         return 'File must be one of: ' + this.types.join(', ');
       }
