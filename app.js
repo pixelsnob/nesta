9d19cf0777233b963ae7da62cdbf9267f5b8896b
Luis A. Echeverria <pixelsnob@gmail.com>
2014-10-29 16:06:13 -0700

    Moved resources back from cms

--- app.js
+++ app.js
@@ -2,30 +2,66 @@
 'use strict';
 
 var
-  views_dir       = __dirname + '/views',
+  port            = 3002,
   express         = require('express'),
   app             = express(),
-  fs              = require('fs');
+  jade_browser    = require('jade-browser'),
+  passport        = require('passport'),
+  _               = require('underscore'),
+  session         = require('express-session'),
+  redis_store     = require('connect-redis')(session),
+  body_parser     = require('body-parser'),
+  fs              = require('fs'),
+  env             = process.env.NODE_ENV || 'development';
 
-app.port = 3002;
+require('./lib/db')('nesta');
+require('./lib/auth');
+require('cms/lib/view_helpers')(app);
+require('./lib/marked')(app);
 
+if (env == 'development') {
+  app.use(express.static('public'));
+}
 
-var env = process.env.NODE_ENV || 'development';
-
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.set('view cache', (env == 'production'));
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(body_parser.json({ extended: true }));
+app.use(require('cookie-parser')());
+app.use(session({
+  store: new redis_store,
+  secret: 'hot~dog',
+  proxy: true,
+  cookie: { secure: (env == 'production') },
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require('csurf')());
+app.locals.pretty = true;
+app.locals._ = _;
 app.use(function(req, res, next) {
+  res.locals.csrf = req.csrfToken();
   res.locals.nav = require('./nav');
+  if (req.isAuthenticated()) {
+    res.locals.user = _.omit(req.user, [ 'password', '__v' ]);
+    // Disable caching if logged in
+    res.setHeader('Cache-Control', 'no-cache');
+  } else {
+    delete res.locals.user;
+  }
   next();
 });
 
-/* Let the CMS configure our app */
+app.use(jade_browser(
+  '/jade.js',
+  [ 'player/*.jade', 'cms/*.jade' ],
+  { root: app.get('views'), minify: (env == 'production') }
+));
 
-require('cms/configure')(app, {
-  view_dir:    __dirname + '/views',
-  jade_paths:  [ 'player/*.jade' ],
-  db_name:     'nesta'
-});
-
-app.use(require('cms/lib/router'));
+app.use(require('cms/router'));
 
 app.use(function(req, res, next) {
   res.status(404).sendFile(__dirname + '/public/404.html');
@@ -45,6 +81,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(app.port);
-console.log('Listening on port ' + app.port);
+app.listen(port);
+console.log('Listening on port ' + port);
+
 
