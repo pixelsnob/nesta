550fc29b7b2405718f62916054d50694a9f68408
Luis Echeverria <pixelsnob@gmail.com>
2016-05-07 21:37:54 -0700

    Cleanup

--- public/js/main/views/app.js
+++ public/js/main/views/app.js
@@ -15,15 +15,6 @@ define([
     initialize: function() {    
       this.playlist_view = new PlayListView;
       this.slideshow_view = new SlideshowView;
-      return;
-      this.$el.find('.content-block .content a').each(function(i) {
-        var regex = new RegExp('^https?:\/\/([^\/]*)'),
-            m     = $(this).attr('href').match(regex);
-        if (m && typeof m[1] != 'undefined' && m[1] !=
-            window.location.hostname) {
-          $(this).attr('target', '_blank');
-        }
-      });
     }
   });
 });
