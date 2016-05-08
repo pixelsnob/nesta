6550e4525e70bbfb3e8ca20098bbe33f057d498b
Luis Echeverria <pixelsnob@gmail.com>
2016-05-07 21:41:20 -0700

    Changes related to social icons, added yelp badge, made header fixed again, other layout tweaks

--- var/mongo_shell.js
+++ var/mongo_shell.js
@@ -1,4 +1,3 @@
-db.pages.update({ path: 'about' }, { $push: { content_blocks: { name: 'dual-images', type: 'markdown', content: 'xxxxx', _id: (new ObjectId) }}, $set: { view: 'cms/pages/about' }});
 db.pages.update({ path: 'about' }, { $pop: { content_blocks: 1 }});
 
 db.pages.insert({ path: 'sounds/what-size-band-do-i-choose', title: 'What Size Band Do I Choose?', description: '', keywords: '', view: 'cms/pages/default', content_blocks: [{ _id: (new ObjectId), name: 'main', content: '# What Size Band Do I Choose?', type: 'markdown' }], body_class: 'band-size' });
@@ -15,3 +14,7 @@ db.pages.insert({ path: 'contact', title: 'Contact Us', description: '', keyword
 db.pages.update({ path: 'sounds/what-size-band-do-i-choose' }, { $set: { body_class: 'sounds' }});
 
 db.pages.update({ path: { $neq: 'index' }}, { $set: { keywords: '', description: '' }}, { multi: true });
+
+db.pages.update({ path: 'about' }, { $push: { content_blocks: { name: 'dual-images', type: 'markdown', content: 'xxxxx', _id: (new ObjectId) }}, $set: { view: 'cms/pages/about' }});
+
+db.pages.update({ path: 'pricing' }, { $push: { content_blocks: { name: 'main-bottom', type: 'markdown', content: 'xxxxx', _id: (new ObjectId) }}});
